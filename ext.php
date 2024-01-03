40e47e59d3c583e408a510148ba38350128c2fba
Matt Friedman <maf675@gmail.com>
2024-01-03 08:51:54 -0800

    Ensure installs only on compatible boards
    Signed-off-by: Matt Friedman <maf675@gmail.com>

--- ext.php
+++ ext.php
@@ -12,10 +12,21 @@ namespace phpbb\webpushnotifications;
 
 /**
  * phpBB Browser Push Notifications Extension base
- *
- * It is recommended to remove this file from
- * an extension if it is not going to be used.
  */
 class ext extends \phpbb\extension\base
 {
+	/**
+	 * {@inheritdoc}
+	 *
+	 * Requires phpBB 3.3.12 due to new template and core events.
+	 * Should not be installed in phpBB 4.0.0-a1 because it already has push notifications.
+	 * Requires PHP 7.3 due to 3rd party libraries included.
+	 */
+	public function is_enableable()
+	{
+		return
+			phpbb_version_compare(PHPBB_VERSION, '3.3.12-dev', '>=') &&
+			phpbb_version_compare(PHPBB_VERSION, '4.0.0-a1', '<') &&
+			phpbb_version_compare(PHP_VERSION_ID, '70300', '>=');
+	}
 }

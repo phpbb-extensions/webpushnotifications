735ac73d841a062a4bee5befb854cbad98d6dd6b
rxu <rxu@mail.ru>
2024-05-14 23:59:57 +0700

    Add ru translation

---
+++ language/ru/info_acp_webpushnotifications.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ *
+ * phpBB Browser Push Notifications. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2023, phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+/**
+ * DO NOT CHANGE
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ » “ ” …
+//
+
+$lang = array_merge($lang, [
+	'ACP_WEBPUSH_EXT_SETTINGS'			=> 'Настройки браузерных push—уведомлений',
+	'ACP_WEBPUSH_REMOVE_WARNING'		=> 'Браузерные push—уведомления теперь встроены в phpBB',
+	'ACP_WEBPUSH_REMOVE_NOTICE'			=> 'Расширение «phpBB Browser Push Notifications» больше не требуется и должно быть удалено.<br>Все системные и пользовательские настройки, связанные с данным расширением, будут перенесены в соответствующие настройки браузерных push—уведомлений конференции автоматически при удалении данного расширения.',
+	'LOG_CONFIG_WEBPUSH'				=> '<strong>Изменены настройки браузерных push—уведомлений</strong>',
+	'LOG_WEBPUSH_MESSAGE_FAIL'			=> '<strong>Не удалось отправить браузерное push—уведомление:</strong> %s',
+	'LOG_WEBPUSH_SUBSCRIPTION_REMOVED'	=> '<strong>Удалена подписка на браузерные push—уведомления:</strong>» %s',
+]);
---
+++ language/ru/webpushnotifications_module_ucp.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ *
+ * phpBB Browser Push Notifications. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2023, phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+/**
+ * DO NOT CHANGE
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = [];
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ » “ ” …
+//
+
+$lang = array_merge($lang, [
+	'NOTIFICATION_METHOD_PHPBB_WPN_WEBPUSH'	=> 'Браузерные push—уведомления',
+	'NOTIFY_WEBPUSH_ENABLE'			=> 'Включить браузерные push—уведомления',
+	'NOTIFY_WEBPUSH_ENABLE_EXPLAIN'	=> 'Включение возможности получения браузерных push—уведомлений.<br>Данная возможность может быть отключена в любое время в настройках браузера, с помощью отписки от браузерных push—уведомлений в личном разделе или путём отключения данной функции ниже.',
+	'NOTIFY_WEBPUSH_SUBSCRIBE'		=> 'Подписаться',
+	'NOTIFY_WEBPUSH_UNSUBSCRIBE'	=> 'Отписаться',
+]);

c0173f6bb9f93fc16fc51eb4c926738eb1dec47f
Matt Friedman <maf675@gmail.com>
2024-05-16 08:00:01 -0700

    Remove attempt at accessing avatar uploads
    Signed-off-by: Matt Friedman <maf675@gmail.com>

--- notification/method/webpush.php
+++ notification/method/webpush.php
@@ -456,44 +456,7 @@ class webpush extends messenger_base implements extended_method_interface
 			$avatar = $matches[1][1] ?? $matches[0][1];
 		}
 
-		if (strpos($avatar, 'download/file.' . $this->php_ext))
-		{
-			$avatar = $this->get_uploaded_avatar($avatar);
-		}
-
 		return preg_replace("#^\\{$this->path_helper->get_web_root_path()}#", $this->get_board_url(), $avatar, 1);
-	}
-
-	/**
-	 * Get the actual path to uploaded avatars
-	 *
-	 * @param string $file
-	 * @return string
-	 */
-	protected function get_uploaded_avatar($file)
-	{
-		$prefix = $this->config['avatar_salt'] . '_';
-		$image_dir = rtrim($this->config['avatar_path'], DIRECTORY_SEPARATOR);
-
-		// Ensure image_dir is relative
-		$image_dir = str_replace(['../', '..\\', './', '.\\'], '', $image_dir);
-		if ($image_dir && ($image_dir[0] === '/' || $image_dir[0] === '\\'))
-		{
-			$image_dir = '';
-		}
-
-		// Extract file extension
-		$ext = pathinfo($file, PATHINFO_EXTENSION);
-
-		// Extract filename using regex
-		$filename = '';
-		if (preg_match('/avatar=(.+?)_/', $file, $matches))
-		{
-			$filename = $matches[1];
-		}
-
-		// Construct final path
-		return $filename ? $this->path_helper->get_web_root_path() . $image_dir . '/' . $prefix . $filename . '.' . $ext : '';
 	}
 
 	/**

bc9f7624222858923b776564b034501e2d3f8e68
Matt Friedman <maf675@gmail.com>
2024-06-15 21:14:45 -0700

    Language updates
    Signed-off-by: Matt Friedman <maf675@gmail.com>

--- language/en/webpushnotifications_module_ucp.php
+++ language/en/webpushnotifications_module_ucp.php
@@ -46,6 +46,5 @@ $lang = array_merge($lang, [
 	'NOTIFY_WEBPUSH_UNSUBSCRIBE'	=> 'Unsubscribe',
 	'NOTIFY_WEBPUSH_SUBSCRIBED'		=> 'Subscribed',
 	'NOTIFY_WEBPUSH_DROPDOWN_TITLE'	=> 'Visit notifications settings to set your preferred push notifications.',
-	'NOTIFY_WEBPUSH_SUB_DENIED'		=> 'Push Notifications Are Denied',
-	'NOTIFY_WEBPUSH_SUB_DENIED_MSG'	=> 'You have denied push notifications from this site. You must allow notifications from this site in your browser settings in order to subscribe to this site.',
+	'NOTIFY_WEBPUSH_DENIED'			=> 'You have denied notifications from this site. To subscribe, please allow notifications in your browser settings.',
 ]);

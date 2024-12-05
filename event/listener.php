dd44ba1483b0c83fd567bf461766c61989261892
Matt Friedman <maf675@gmail.com>
2024-12-05 08:10:02 -0800

    Refactor trim method for readability

--- event/listener.php
+++ event/listener.php
@@ -366,6 +366,8 @@ class listener implements EventSubscriberInterface
 	 */
 	protected function trim_shortname($name)
 	{
-		return htmlspecialchars(utf8_substr(ext::decode_entities($name, ENT_QUOTES), 0, 12), ENT_QUOTES, 'UTF-8');
+		$decoded = ext::decode_entities($name, ENT_QUOTES);
+		$trimmed = utf8_substr($decoded, 0, 12);
+		return htmlspecialchars($trimmed, ENT_QUOTES, 'UTF-8');
 	}
 }

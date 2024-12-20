6900da034d917487e93e2ae4e976bcdec61c20d3
Matt Friedman <maf675@gmail.com>
2024-12-20 06:48:40 -0800

    Does this fix tests?

--- manifest.php
+++ manifest.php
@@ -14,7 +14,7 @@ use phpbb\webpushnotifications\ext;
 /**
 * @ignore
 **/
-define('IN_PHPBB', true);
+const IN_PHPBB = true;
 $phpbb_root_path = ((defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : './') . '../../../';
 $phpEx = substr(strrchr(__FILE__, '.'), 1);
 include($phpbb_root_path . 'common.' . $phpEx);

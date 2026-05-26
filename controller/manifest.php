b2c8645a208853e9e9856b8193028fac78f1e981
Matt Friedman <maf675@gmail.com>
2026-05-25 21:07:36 -0700

    Fix defaults in manifest

--- controller/manifest.php
+++ controller/manifest.php
@@ -61,8 +61,8 @@ class manifest
 			'orientation'	=> 'portrait',
 			'start_url'		=> $start_url,
 			'scope'			=> $scope,
-			'theme_color'	=> $this->user->style['pwa_theme_color'] ?? ext::PWA_THEME_COLOR,
-			'background_color' => $this->user->style['pwa_bg_color'] ?? ext::PWA_BG_COLOR,
+			'theme_color'	=> !empty($this->user->style['pwa_theme_color']) ? $this->user->style['pwa_theme_color'] : ext::PWA_THEME_COLOR,
+			'background_color' => !empty($this->user->style['pwa_bg_color']) ? $this->user->style['pwa_bg_color'] : ext::PWA_BG_COLOR,
 		];
 
 		if (!empty($this->config['pwa_icon_small']) && !empty($this->config['pwa_icon_large']))
